982cd9bd50792ccb8d5e7d051b30648b2fccf352
maxir <maxirinkel@gmail.com>
2023-10-04 21:24:22 +0200

    first commit

---
+++ mail.php
@@ -0,0 +1,40 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "maxirinkel@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo'
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Vielen Dank für ihr Interesse.Ihr Anleigen wird schnellstmöglich bearbeitet</h1>
+        <p class="back">Zurück zur <a href="http://slowinetech.de">Homepage</a>.</p>
+        
+    </div>
+</body>
+</html>
+
+
+
+';
+
+
+?>
